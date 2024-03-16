347595c677fc387c60b4686215565c6b03e565df
Stefan Dimitrov <117269448+StefanDimitrow@users.noreply.github.com>
2024-03-16 19:10:28 +0200

    Added some stylization to 'Books'

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Characters } from 'src/app/service/api-dataservice/dataservice-interface';
 import { LotrapiDataserviceService } from 'src/app/service/api-dataservice/lotrapi-dataservice.service';
 
 @Component({
@@ -8,13 +9,15 @@ import { LotrapiDataserviceService } from 'src/app/service/api-dataservice/lotra
 })
 export class BooksComponent implements OnInit {
   books: any[] = [];
+  characters: Characters[] =[];
+
 
   constructor(private dataService: LotrapiDataserviceService) {}
   ngOnInit(): void {
     this.dataService.getBooks().subscribe((data) => {
       this.books = data.results;
-
       
+
     });
   }
 }
--- src/app/service/api-dataservice/dataservice-interface.ts
+++ src/app/service/api-dataservice/dataservice-interface.ts
@@ -1,50 +1,54 @@
+export interface ApiResponse {
+  count: number;
+  results: Books[];
+  links?: {
+    first?: string;
+    last?: string;
+    prev?: string | null;
+    next?: string | null;
+  };
+}
 
-
-export interface ApiResponse{
-    count:number;
-    results: Books[];
-    links?: {
-        first?:string;
-        last?:string;
-        prev?:string | null;
-        next?:string | null
-    };
-}
-export interface Books {
-    id:number;
-    title:string;
-    author: string;
-    publication_date:string;
-    url:any;
-}
-export interface Films {
-    id: number;
-    title:string;
-    release_date:string;
-    director:string[];
-    producer:string[];
-}
 export interface Characters {
-    id:number;
-    name:string;
-    height:string;
-    hair_color:string;
-    eye_color:string;
-    gender:string;
-    date_of_birth: string;
-    date_of_death: string;
-    weapons: string[];
+hairColor: any;
+eyeColor: any;
+dateOfBirth: any;
+dateOfDeath: any;
+  id: number;
+  name: string;
+  height: string;
+  hair_color: string;
+  eye_color: string;
+  gender: string;
+  date_of_birth: string;
+  date_of_death: string;
+  weapons: string[]
+  url:any;
 }
 export interface Species {
-    name:string;
-    
+  name: string;
 }
 export interface Realms {
-    name:string;
-    capital:string;
-    founded_date:string;
-    type:string;
-    location:string;
-    inhabitants:string[];
-
+  name: string;
+  capital: string;
+  founded_date: string;
+  type: string;
+  location: string;
+  inhabitants: string[];
+}
+export interface Books {
+name: string;
+publicationDate: string;
+  id: number;
+  title: string;
+  author: string;
+  characters: string[];
+  publication_date: string;
+}
+export interface Films {
+  id: number;
+  title: string;
+  release_date: string;
+  director: string[];
+  producer: string[];
 }
