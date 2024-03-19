64a6f9c185ae20a88b591a09ee7ae5d36c8744e4
StefanDimitrow <stefandimitrov94bg@gmail.com>
2024-03-19 16:03:31 +0200

    Set the 'Login'  and 'SignUp' buttons to hiden when user is logged in, and the Logout button to appear when user is logged in

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/service/authentication/auth.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
 
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated().subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+      
+    });
+  }
+  signout() {
+    this.authService.signout();
+  }
 }
--- src/app/service/authentication/auth.service.ts
+++ src/app/service/authentication/auth.service.ts
@@ -1,42 +1,48 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  constructor(private fireaut: AngularFireAuth, private router: Router) {}
 
-  constructor(private fireaut: AngularFireAuth, private router: Router) { }
+  isAuthenticated(): Observable<boolean> {
+    return this.fireaut.authState.pipe(map(user => !!user));
+  }
 
   login(email: string, password: string) {
     this.fireaut.signInWithEmailAndPassword(email, password).then(() => {
       localStorage.setItem('token', 'true');
-      this.router.navigate(['/home'])
+      this.router.navigate(['/home']);
     }, err => {
       alert('Something went wrong!');
       this.router.navigate(['/login']);
-    })
+    });
   }
-
 
   register(email: string, password: string) {
     this.fireaut.createUserWithEmailAndPassword(email, password).then(() => {
-      alert('Registration is complete!')
-      this.router.navigate(['/login'])
+      alert('Registration is complete!');
+      this.router.navigate(['/login']);
     }, err => {
       alert('Something went wrong!');
       this.router.navigate(['/register']);
-    })
+    });
   }
 
   signout() {
     this.fireaut.signOut().then(() => {
       localStorage.removeItem('token');
-      this.router.navigate(['/login'])
+      this.router.navigate(['/login']);
     }, err => {
       alert(err.message);
-    })
+    });
   }
-
 }
+
+
+
