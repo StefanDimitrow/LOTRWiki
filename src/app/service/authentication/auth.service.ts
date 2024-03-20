50341509a97a6869baacb43ad0c32c7bf5acf24f
StefanDimitrow <stefandimitrov94bg@gmail.com>
2024-03-20 16:06:41 +0200

    Some changes (not important)

--- src/app/service/authentication/auth.service.ts
+++ src/app/service/authentication/auth.service.ts
@@ -11,8 +11,8 @@ export class AuthService {
   constructor(private fireaut: AngularFireAuth, private router: Router) {}
 
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    _route: ActivatedRouteSnapshot,
+    _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.isAuthenticated().pipe(
       take(1),
       map(authenticated => {
@@ -33,7 +33,7 @@ export class AuthService {
     this.fireaut.signInWithEmailAndPassword(email, password).then(() => {
       localStorage.setItem('token', 'true');
       this.router.navigate(['/home']);
-    }, err => {
+    }, _err => {
       alert('Something went wrong!');
       this.router.navigate(['/login']);
     });
@@ -43,7 +43,7 @@ export class AuthService {
     this.fireaut.createUserWithEmailAndPassword(email, password).then(() => {
       alert('Registration is complete!');
       this.router.navigate(['/login']);
-    }, err => {
+    }, _err => {
       alert('Something went wrong!');
       this.router.navigate(['/register']);
     });
