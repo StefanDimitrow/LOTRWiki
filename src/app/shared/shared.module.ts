51aaef83f5644af2a3871a91929258fc08a34132
StefanDimitrow <stefandimitrov94bg@gmail.com>
2024-04-02 15:33:24 +0300

    Removed an unused component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LikeBtnComponent } from './like-btn/like-btn.component';
-import { NavBtnComponent } from './nav-btn/nav-btn.component';
+
 
 
 
 @NgModule({
   declarations: [
     LikeBtnComponent,
-    NavBtnComponent
+    
   ],
   imports: [
     CommonModule
   ],
-  exports: [LikeBtnComponent,NavBtnComponent]
+  exports: [LikeBtnComponent,]
 })
 export class SharedModule { }
