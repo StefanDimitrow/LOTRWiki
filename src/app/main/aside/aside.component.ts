140b2322ce82c124bbd850d1c9ad70b63804db71
StefanDimitrow <stefandimitrov94bg@gmail.com>
2024-04-03 13:41:58 +0300

    Added some animations on the home page

--- src/app/main/aside/aside.component.ts
+++ src/app/main/aside/aside.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition,state } from '@angular/animations';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.css'],
+  animations: [
+    trigger('zoomIn', [
+      state('normal', style({
+        transform: 'scale(1)',
+      })),
+      state('hovered', style({
+        transform: 'scale(1.1)', // Zoom in effect
+      })),
+      transition('normal => hovered', animate('200ms ease-in')),
+      transition('hovered => normal', animate('200ms ease-out')),
+    ]),
+  ],
 })
-export class AsideComponent {}
+export class AsideComponent {
+  imageState: string = 'normal'; // Initial state
+}
